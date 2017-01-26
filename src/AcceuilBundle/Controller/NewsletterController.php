945e5011d69c5d003026210750e1cb1f41c979c3
Madlax <yanistam@hotmail.fr>
2017-01-26 01:50:39 +0100

    mashallah ça fonctionne

--- src/AcceuilBundle/Controller/NewsletterController.php
+++ src/AcceuilBundle/Controller/NewsletterController.php
@@ -3,11 +3,15 @@
 namespace AcceuilBundle\Controller;
 
 use AcceuilBundle\Entity\Mail;
+use AcceuilBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\Model\UserInterface;
 use Hip\MandrillBundle\Message;
 use Hip\MandrillBundle\Dispatcher;
 
@@ -30,8 +34,11 @@ class NewsletterController extends Controller
 
         $mails = $em->getRepository('AcceuilBundle:Mail')->findAll();
 
+        $user = $this->getUser();
+
         return $this->render('mail/index.html.twig', array(
             'mails' => $mails,
+            'user' => $user
         ));
     }
 }
