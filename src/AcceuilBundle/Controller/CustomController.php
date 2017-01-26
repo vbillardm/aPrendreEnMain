b776725f6cb9e4e629fd025ac76e1e5d9e4672e3
Ivan PRUNIER <ivan.prunier7@gmail.com>
2017-01-26 10:19:30 +0100

    Finishes customs v2

--- src/AcceuilBundle/Controller/CustomController.php
+++ src/AcceuilBundle/Controller/CustomController.php
@@ -17,32 +17,47 @@ class CustomController extends Controller
 {
 
     /**
-     * Lists all custom entities.
-     *
-     * @Route("/", name="custom_index")
-     * @Method("GET")
-     */
-    public function indexAction()
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $news = $em->getRepository('AcceuilBundle:News')->findAll();
-
-        return $this->render('news/index.html.twig', array(
-            'news' => $news,
-        ));
-    }
-
-
-    /**
      * Displays a form to edit an existing custom entity.
      *
      * @Route("/{id}/edit", name="custom_edit")
      * @Method({"GET", "POST"})
      */
-    public function editAction(Request $request, News $news)
+    public function editAction(Request $request, Custom $custom)
     {
+        $editForm = $this->createForm('AcceuilBundle\Form\CustomType', $custom);
+        $editForm->handleRequest($request);
 
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+          $this->getDoctrine()->getManager()->flush();
+
+          return $this->redirectToRoute('acceuil_admin_index');
+        }
+
+        return $this->render('custom/edit.html.twig', array(
+            'custom' => $custom,
+            'edit_form' => $editForm->createView(),
+            'user' => $this->getUser(),
+        ));
+    }
+
+    /**
+     * Deletes a custom entity.
+     *
+     * @Route("/{id}", name="custom_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Custom $custom)
+    {
+        $form = $this->createDeleteForm($custom);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($custom);
+            $em->flush($custom);
+        }
+
+        return $this->redirectToRoute('acceuil_admin_index');
     }
 }
 
---
+++ src/AcceuilBundle/Form/CustomType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AcceuilBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CustomType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('value');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AcceuilBundle\Entity\Custom'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'acceuilbundle_custom';
+    }
+
+
+}
