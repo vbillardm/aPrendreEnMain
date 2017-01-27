0d1f80c0b4a77ab4b77dfd19158bf2a72782539c
Madlax <yanistam@hotmail.fr>
2017-01-27 03:37:30 +0100

    List deroulante (TODO : styles)

---
+++ web/js/list.js
@@ -0,0 +1,15 @@
+$( "#context" ).click(function() {
+    $("#contextContainer").toggle(500);
+});
+
+$("#objectifchiffres").click(function(){
+    $("#objectifsContainer").toggle(500);
+});
+
+$("#coutProjet").click(function(){
+    $("#coutContainer").toggle(500);
+});
+
+$("#realisationDate").click(function(){
+    $("#dateContainer").toggle(500);
+});
